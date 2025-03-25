f7e9884b2a5b58a20bd57af0e1891f49b7b79ad9
yaxalponce <37907862+yaxalponce@users.noreply.github.com>
2025-03-25 16:32:31 +0100

    Update plotMappability.R
    Add violin for reads mapping Type 1 and Type 2 references

--- scripts/plotMappability.R
+++ scripts/plotMappability.R
@@ -12,6 +12,14 @@ Type2_Strains <- scan("${WORKINGDIR}/listEbvType2.txt", what = "character")
 coords <- which(EBVmappabilityT1$Strain %in% Type2_Strains)
 EBVmappabilityT1$Type[coords] <- c("EBV Type 1 reference vs EBV Type 2")
 
+# Mappability of reads mapping to T1 and T2
+
+EBVmappabilityT1T2 <- fread("Mappability_EBVT1_T2.tsv", header = FALSE, sep = "\t", col.names = c("Strain", "Mappability"))
+EBVmappabilityT1T2$Type <- rep("z_EBV Type 1", nrow(EBVmappabilityT1T2))
+
+coords <- which(EBVmappabilityT1T2$Strain %in% Type2_Strains)
+EBVmappabilityT1T2$Type[coords] <- c("z_EBV Type 2")
+
 # Read mappability against reference EBV Type 2
 setwd("${WORKINGDIR}/EBV_T2/resultsEBVT2")
 
@@ -21,14 +29,16 @@ EBVmappabilityT2$Type <- rep("EBV Type 2 reference vs EBV Type 1", nrow(EBVmappa
 coords <- which(EBVmappabilityT2$Strain %in% Type2_Strains)
 EBVmappabilityT2$Type[coords] <- c("EBV Type 2 reference vs EBV Type 2")
 
-# Merge the two datasets and plot them together
-EBVmappability <- rbind(EBVmappabilityT1, EBVmappabilityT2)
+# Merge the two data sets and plot them together
+EBVmappability <- rbind(EBVmappabilityT1, EBVmappabilityT2, EBVmappabilityT1T2)
 
 pdf("${WORKINGDIR}/Mappability_EBVT1_EBVT2.pdf", height = 7, width = 11)
   ggplot(EBVmappability, aes(x=Type, y=Mappability, fill=Type)) +
-    geom_violin() +
-    labs(title = "Mappability against EBV type 1 and type 2",
-         x = "",
+    geom_violin() +  
+    labs(title = "Mappability against EBV type 1 and type 2", 
+         x = "", 
          y = "Mappability") +
-    theme(axis.text.x = element_text(size = 7, angle = 0, hjust = 0.5))
+    theme(axis.text.x = element_text(size = 7, angle = 0, hjust = 0.5), 
+          legend.position = "bottom",
+          legend.box = "vertical")
 dev.off()
